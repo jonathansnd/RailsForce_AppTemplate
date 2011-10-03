92ef057a044d28dc7d433eba6ecdaedb3713eacd
Jonathan Rico <jonathan_rico@softwarenextdoor.com>
2011-10-02 18:38:03 -0700

    Fixed issue with ominauth strategy

--- lib/forcedotcom.rb
+++ lib/forcedotcom.rb
@@ -15,6 +15,37 @@ module OmniAuth
 
         super(app, :forcedotcom, consumer_key, consumer_secret, client_options, options, &block)
       end
-      
+
+      def auth_hash
+        data = user_data
+        OmniAuth::Utils.deep_merge(super, {
+            'uid' => @access_token['id'],
+            'credentials' => {
+              "instance_url" => @access_token['instance_url'],
+              "refresh_token" => @access_token.refresh_token,
+              "consumer_key" => @access_token.client.id,
+              "consumer_secret" => @access_token.client.secret
+            },
+            'extra' => {'user_hash' => data},
+            'user_info' => {
+              'email' => data['email'],
+              'name' => data['display_name']
+            }
+          })
+      end
+
+
+      def user_data
+        @data ||= MultiJson.decode(@access_token.get(@access_token['id']))
+      rescue ::OAuth2::HTTPError => e
+        if e.response.status == 302
+          @data ||= MultiJson.decode(@access_token.get(e.response.headers['location']))
+        else
+          raise e
+        end
+      end
+    end
+    
+
   end
 end
